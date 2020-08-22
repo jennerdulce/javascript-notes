dcb31767606b3ed07df9d3f1d25b3111c431da08
Jenner Dulce <jennerdulce@gmail.com>
2020-08-22 16:59:37 -0700

    button notes

---
+++ app.js
@@ -0,0 +1,7 @@
+function helloThere() {
+    document.getElementById('blank').innerHTML = 'Yo hey whats up:)'
+}
+
+function goodbyeThere() {
+    document.getElementById('blank').innerHTML = 'Oh goodbye, it was nice meeting you. call me sometime baby'
+}
