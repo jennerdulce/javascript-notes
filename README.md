267a66e6360a17e091d4d593c3d653ca6345c1aa
Jenner Dulce <jennerdulce@gmail.com>
2020-08-23 17:56:58 -0700

    updated notes

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # javascript-notes
+
+- different ways to display messages:
+    - `.innerHTML`: writing into an HTML element
+    - `document.write()`: HTML output
+    - `window.alert()`: alert box
+    - `console.log()`: into the terminal
+
+- `document.getElementById("id")` accesses an HTML element
+
+- Using `innerHTML`
+    - defines the HTML content
+    - changing the innerHTML property of an HTML element is a common way to display data
+
+- `document.write()`
+    - for testing purposes only, it is conveninet to use `document.write()`
+    - will output where ever the script tag is
+
+- `console.log()`
+    - used for debugging
+    - do not forget to write a label
+    - `console.log('label', x)
+
+- `window.print()`
+    - opens up print for the current page
+
+
+- Literals: `console.log('this is a literal')`
+- Variable: `var x = 'this is a variable`
+    - `console.log(x)`
--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 function helloThere() {
-    document.getElementById('blank').innerHTML = 'Yo hey whats up:)'
+    document.getElementById('blank').innerHTML = 'Yo hey whats up:),'
 }
 
 function goodbyeThere() {
-    document.getElementById('blank').innerHTML = 'Oh goodbye, it was nice meeting you. call me sometime baby'
+    document.getElementById('blank').innerHTML = 'Oh goodbye, it was nice meeting you. call me sometime baby,'
 }
 
 function alertUser() {
     window.alert("ALERT!! YOU AREN'T SUPPOSED TO PRESS THIS BUTTON")
 
 }
+
+function hateYou() {
+    document.getElementById('word').innerHTML =
+    'I hate you so much.'
+}
+
+function resetButton() {
+    document.getElementById('blank').innerHTML =
+    '_____ ,'
+    document.getElementById('word').innerHTML =
+    '_____'
+}
+
+var age = 27;
+var person = 'Jenner Dulce'
+var hobby = 'surfing'
+
+document.getElementById('demo').innerHTML =
+'hi my name is ' +  name + ' i am ' + age + ' years old. my favorite hobby is ' + hobby;
