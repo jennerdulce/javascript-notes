dca0354aabd605d14d533624e6c991648c6ed618
Jenner Dulce <jennerdulce@gmail.com>
2020-08-26 11:40:48 -0700

    added string files

---
+++ string.js
@@ -0,0 +1,52 @@
+// slice()
+var slicing = function(num1, num2) {
+    var alter = str.slice(num1, num2);
+    document.getElementById('demo1').innerHTML = 
+    '<b>slicing example:</b> ' + alter;
+}
+var str = "red, blue, green, red, orange, yellow, red";
+slicing(5)
+// slicing(5, 9)
+// slicing(-5)
+
+// ====================================================
+
+// replace()
+
+var str2 = "red, blue, green, red, orange, yellow, red";
+
+function replacing() {
+    // var x = str2.replace('red', 'WHITE') // replaces only first match
+
+    // var x = str2.replace(/RED/i, 'WHITE'); // makes insensitive
+
+    // var x = str2.replace(/red/g, 'wWHITE'); // replaces all matches
+
+    var x = str2.replace(/red/gi, 'WHITE'); // replaces all matches AND insensitive
+    
+    document.getElementById('demo2').innerHTML =
+    '<b>replacing string contents:</b> ' + x;
+}
+
+replacing()
+
+
+
+// ====================================================
+
+// toUpperCase() & toLowerCase()
+
+
+function makeUpper(sentence) {
+    text1 = sentence.toUpperCase();
+    document.getElementById('demo3').innerHTML =
+    '<b>make upper case:</b> ' + text1;
+}
+makeUpper('make this sentence upper case!')
+
+function makeLower(sentence) {
+    text1 = sentence.toLowerCase();
+    document.getElementById('demo4').innerHTML =
+    '<b> make lower case: </b>' + text1;
+}
+makeLower('THIS SENTENCE IS ORIGINALLY IN ALL CAPS')
---
+++ string.md
@@ -0,0 +1,35 @@
+- `.length`
+- `indexOf('word that you want to find', #)`
+    - finds first index occurance of the string
+- `search()` is similar to `indexOf()` however, search cannot accept a second position argument
+- `lastIndexOf('word that you want to find', #)`
+    - '#' = what index to start the search
+
+### extracting string parts
+- 3 ways:
+    - `slice(start, end)`
+    - `substring(start, end)`
+        - similar to `slice()` but cannot accept negatives
+    - `substr(start, length)` 
+        - similar to `slice()` but second number specifices how many additional characters you want
+
+### replacing string content
+- `replace()`: replaces a specific value with another value in a string
+    - replaces only the first match
+    - is case sensitive
+    - `replace(/VARIABLE/i, 'newvariable')`
+        - !case sensitive
+    - `replace(/variable/g, 'newvariable')`
+        - replaces ALL matches
+
+### upper and lower
+- `stringvar.toUpperCase()`
+- `stringvar.toLowerCase()`
+
+### extracting string characters
+- `charAt(index)`
+- `charCodeAt(index)`
+    - property access[]
+
+### extra methods
+- `trim()`: removes white space
